17032aaf1f2c66c9443b3286e4792b54163b2a07
Cory Lamontagne <corylamontagne@Yahoo.com>
2020-12-05 21:22:32 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -42,26 +42,18 @@ int main()
     //Day 4 //
     //std::map<int, std::vector<std::string>> data = AOC::GroupParsedFileByTokens(AOC::ParseFileByLine("Input/P4-input.txt"), std::vector<char>{' '}, true);
 
-    ////Problem 1
-    //std::map<std::string, std::string> requiredFields{  {"byr", ""},
-    //                                                    {"iyr", ""},
-    //                                                    {"eyr", ""},
-    //                                                    {"hgt", ""},
-    //                                                    {"hcl", ""},
-    //                                                    {"ecl", ""},
-    //                                                    {"pid", ""}};
+    //std::map<std::string, std::string> requiredFields { {"byr", "19[2-8][0-9]|199[0-9]|200[0-2]"},
+    //                        {"iyr", "201[0-9]|2020"},
+    //                        {"eyr", "202[0-9]|2030"},
+    //                        {"hgt", "^((1([5-8][0-9]|9[0-3]))cm|(59|6[0-9]|7[0-6])in)"},
+    //                        {"hcl", "#[0-9a-f]{6}"},
+    //                        {"ecl", "amb|blu|brn|gr[yn]|hzl|oth"},
+    //                        {"pid", "^[0-9]{9}$"} };
 
+    ////Problem 1
     //std::cout << Day4::CountValidPassports(data, requiredFields, false) << std::endl;
 
     ////Problem 2
-    //requiredFields = {  {"byr", "19[2-8][0-9]|199[0-9]|200[0-2]"},
-    //                    {"iyr", "201[0-9]|2020"},
-    //                    {"eyr", "202[0-9]|2030"},
-    //                    {"hgt", "^((1([5-8][0-9]|9[0-3]))cm|(59|6[0-9]|7[0-6])in)"},
-    //                    {"hcl", "#[0-9a-f]{6}"},
-    //                    {"ecl", "amb|blu|brn|gr[yn]|hzl|oth"},
-    //                    {"pid", "^[0-9]{9}$"} };
-
     //std::cout << Day4::CountValidPassports(data, requiredFields, true) << std::endl;
 
     //Day 3//
